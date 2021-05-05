aab805c667fce6b094dc188775fd38eaaf0f1044
thalesludwig <thales.ludwig@gmail.com>
2021-05-05 11:16:27 -0300

    Added persistor

--- src/config/store.js
+++ src/config/store.js
@@ -1,8 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import themeReducer from "../slices/themeSlice";
 
-export default configureStore({
-  reducer: {
-    theme: themeReducer,
-  },
+const rootReducer = combineReducers({
+  theme: persistReducer({ key: "theme", storage }, themeReducer),
 });
+
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: [],
+});
+
+export const persistor = persistStore(store);
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./containers/App";
-import store from "./config/store";
+import store, { persistor } from "./config/store";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading="Loading" persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
